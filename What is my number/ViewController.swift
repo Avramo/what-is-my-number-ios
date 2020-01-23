879ba401afe314df615a8ee50b3fdf9067efdfba
elnatan <elnatan@ravtech.co.il>
2020-01-23 15:18:15 +0200

    Initial Commit

---
+++ What is my number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What is my number
+//
+//  Created by admin on 23/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
